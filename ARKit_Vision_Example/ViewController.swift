7853207043ff5ec3ee0bf36912808a17f50b086b
Eldar Eliav <eldar@bringg.com>
2021-02-06 16:55:40 +0200

    Initial Commit

---
+++ ARKit_Vision_Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARKit_Vision_Example
+//
+//  Created by Eldar Eliav on 06/02/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
